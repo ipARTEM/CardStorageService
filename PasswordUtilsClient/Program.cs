464f0af3349bd2daf8e5d75e48ddaa215adbd66c
Artem Khimin <a.khimin@yandex.ru>
2022-09-18 14:37:23 +0300

    Added CreatePasswordHash

---
+++ PasswordUtilsClient/Program.cs
@@ -0,0 +1,8 @@
+﻿
+
+using CardStorageService.Utils;
+
+var result = PasswordUtils.CreatePasswordHash("12345");
+Console.WriteLine(result.passwordSalt);
+Console.WriteLine(result.passwordHash);
+Console.ReadKey(true);
