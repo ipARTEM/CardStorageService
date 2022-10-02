386c2558e7d8a348a0a5c708500902e2e7c2c6e8
a.khimin <a.khimin@yandex.ru>
2022-10-02 22:41:15 +0300

    Added gRPC

---
+++ CardStorageClient/Program.cs
@@ -0,0 +1,41 @@
+﻿
+
+
+using Grpc.Net.Client;
+using static CardStorageServiceProtos.CardService;
+using static CardStorageServiceProtos.ClientService;
+
+AppContext.SetSwitch("System.Net.Http.SocketHttpHandler.Http2UnencryptedSupport", true);
+
+//CardServiceClient
+//ClientServiceClient
+
+using var channel = GrpcChannel.ForAddress("http://localhost:5001");
+
+ClientServiceClient clientService = new ClientServiceClient(channel);
+
+var createClientResponse = clientService.Create(new CardStorageServiceProtos.CreateClientRequest
+{
+    FirstName = "Artem",
+    SurName = "Khimin",
+    Patronymic = "Pavlovich"
+});
+
+Console.WriteLine($"Client {createClientResponse.ClientId} created successfully.");
+
+CardServiceClient cardService = new CardServiceClient(channel);
+
+var getByClientIdResponse = cardService.GetByClientId(new CardStorageServiceProtos.GetByClientIdRequest
+{
+    ClientId = 1
+});
+
+Console.WriteLine("Cards:");
+Console.WriteLine("======");
+
+foreach (var card in getByClientIdResponse.Cards)
+{
+    Console.WriteLine($"{card.CardNo}; {card.Name}; {card.CVV2}; {card.ExpDate}");
+}
+
+Console.ReadKey();
